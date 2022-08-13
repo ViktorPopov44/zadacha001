faa05405fafda480e21048ef26ee5f3ea90886fe
ViktorPopov44 <110253043+ViktorPopov44@users.noreply.github.com>
2022-08-13 14:23:04 +0300

    Add files via upload

---
+++ zadacha003/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введи номер дня недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumber) {
+  if (dayNumber == 6 || dayNumber == 7) {
+  Console.WriteLine("выходной");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) {
+    Console.WriteLine("нет столько дней в неделе");
+  }
+  else Console.WriteLine("не выходной");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
